b4884e75f7e04f40acb98e501647f7076cdf5c19
KudesnikRaf <kolesnik.akk@gmail.com>
2023-11-27 23:55:52 +0300

    Upd page-loader.js make a CLI program

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+import { Command } from 'commander'; 
 import loadPage from '../src/index.js';
 
-loadPage();
+const program = new Command();
+
+program 
+  .name('page-loader')
+  .description('Usage: page-loader')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<url>')
+  .action((url, options) => {
+    loadPage(url);
+    console.log(options);
+  });
+
+  program.parse();
+
